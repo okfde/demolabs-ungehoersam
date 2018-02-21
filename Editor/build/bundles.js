46d95e672fd3b2a47e74759fa83e35d03bdb2b3d
sp4c31nv4d3r <ml_entertainment@hotmail.com>
2018-02-21 12:11:59 +0100

    Add files via upload

---
+++ Editor/build/bundles.js
@@ -0,0 +1,61 @@
+module.exports = {
+  "bundles": {
+    "dist/app-build": {
+      "includes": [
+        "[**/*.js]",
+        "**/*.html!text",
+        "**/*.css!text"
+      ],
+      "options": {
+        "inject": true,
+        "minify": true,
+        "depCache": true,
+        "rev": false
+      }
+    },
+    "dist/vendor": {
+      "includes": [
+        "localforage",
+		"nprogress",
+		"nprogress/nprogress.css!text",
+		"simple-markdown",
+        "horsey",
+        "horsey/dist/horsey.css!text",
+      ],
+      "options": {
+        "inject": true,
+        "minify": true,
+        "depCache": false,
+        "rev": false
+      }
+    },
+    "dist/aurelia": {
+      "includes": [
+        "aurelia-framework",
+        "aurelia-bootstrapper",
+        "aurelia-fetch-client",
+        "aurelia-router",
+        "aurelia-animator-css",
+        "aurelia-templating-binding",
+        "aurelia-polyfills",
+        "aurelia-templating-resources",
+        "aurelia-templating-router",
+        "aurelia-loader-default",
+        "aurelia-history-browser",
+        "aurelia-logging-console",
+        "aurelia-pal-browser",
+		"aurelia-dialog",
+        "bootstrap",
+        "bootstrap/css/bootstrap.css!text",
+        "fetch",
+        "jquery"
+      ],
+      "options": {
+        "inject": true,
+        "minify": true,
+        "depCache": false,
+        "rev": false
+      }
+    }
+  }
+};
